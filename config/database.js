f91ad79a591778fd8d2bf55180bbdcea3a0ddc6d
Unknown <etienne.guillou1@gmail.com>
2018-10-29 17:50:02 -0400

    Réparation de mes bêtises

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// 27017 is the default port number.
+module.exports = {
+    //database: 'mongodb://localhost:27017/amigo'
+    database:'mongodb://heroku_8n0twrjd:evua71econfpgtt5hdcc1hj1qr@ds137801.mlab.com:37801/heroku_8n0twrjd'
+}

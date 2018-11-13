eb919e23f27d60823f0012854f463ec954efcf71
Unknown <etienne.guillou1@gmail.com>
2018-11-12 23:00:27 -0500

    Bugfix - lien vers l'api (2)

--- angular-src/dist/angular-src/main.js
+++ angular-src/dist/angular-src/main.js
@@ -1803,7 +1803,7 @@ __webpack_require__.r(__webpack_exports__);
 };*/
 var environment = {
     production: true,
-    api: 'https://travel-express-project.herokuapp.com'
+    api: 'https://travel-express-project.herokuapp.com/api'
 };
 /*
  * For easier debugging in development mode, you can import the following file
